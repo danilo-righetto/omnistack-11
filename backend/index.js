a31afaa77be1a3565e55401c0a0ada717eca9d61
Danilo Righetto <danilonewtrue@gmail.com>
2020-04-03 23:54:09 -0300

    Trabalhando com parametros no node

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,49 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.send('Hello World - Danilo - Omnistack 11');
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Métodos HTTP:
+  * 
+  * GET: Buscar uma informação do back-end
+  * POST: Criar uma informação no back-end
+  * PUT: Alterar uma informação no back-end
+  * DELETE: Deletar uma informação no back-end
+  */
+
+  /**
+   * Tipos de parâmetros
+   * 
+   * Query Params: Parâmetros nomeados enviados na rota após o '?' (Filtros, paginação)
+   * Route Params: Parâmetros utilizados para identificar recursos
+   * Request Body: 
+   */
+
+   /**
+    * SQL: MySQL, SQLITE, PostegreSQL, Oracle, Microsoft SQL Server
+    * NoSQL: MongoDB, CouchDB, etc
+    */
+
+app.get('/users', (request, response) => {
+    const params = response.query;
+    const body = request.body;
+
+    console.info(params);
+    console.info(body);
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Diego Fernandes'
+    });
+});
+
+app.get('/user/:id', (request, response) => {
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Diego Fernandes'
+    });
 });
 
 app.listen(3333);
